a12a6d0ddf42abccda10e3693960f5d206f9bfb1
alexbulintis <alexbulintis@icloud.com>
2016-10-16 00:38:57 +0300

    modified ip

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.get("*", function(req, res){
   var data = {
-    "ip": req.ip,
+    "ip": req.headers['x-forwarded-for'] || req.connection.remoteAddress,
     "lang": req.headers['accept-language'].split(',')[0],
     "os": req.headers['user-agent'].split(')')[0].split('(')[1]
   };
